b7eb92d91c02584f660d9ca2e7b54d516d3c2f26
Nethsara Prabash <138272058+nethsaraPrabash@users.noreply.github.com>
2024-10-09 23:18:11 +0530

    couter_instance

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Countdown from './Countdown'
+import Round from './Round'
 
 const Dashboard = () => {
   return (
     <div>
+        <Round/>
         <Countdown/>
     </div>
     
